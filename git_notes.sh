37c48eb35a1c0a4abbeebd53b20d9f0b36277c2a
ayo1009 <ajape.ayo@gmail.com>
2023-03-06 01:31:06 +0000

    this is my devOps commit

---
+++ git_notes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+How to Reset A Local Git Branch to Remote?
+You may have been working on your local branch, making various changes and modifications to a project, and you concluded that those changes you made are no longer needed.
+
+You want to remove them and reset the branch to the remote branch.
+
+On top of that, another developer may have made changes and pushed them to the remote branch, so you need to fetch those latest changes from the remote repository to be up to date.
+
+The steps you need to take to achieve this are the following:
+
+Save the current state of your local branch (optional).
+Fetch the latest version of the code from the remote.
+Reset the local branch.
+Clean up files (optional).
+
